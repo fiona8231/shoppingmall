174aa0547b9b2b6561578201170a5515f96ea08e
Yue <fionaxuyue8231@gmail.com>
2020-12-03 23:10:18 +0100

    clean up error

--- mall-production/src/main/java/com/fiona/mall/production/service/impl/AttrServiceImpl.java
+++ mall-production/src/main/java/com/fiona/mall/production/service/impl/AttrServiceImpl.java
@@ -1,13 +1,13 @@
 package com.fiona.mall.production.service.impl;
 
 import com.fiona.mall.common.utils.PageUtils;
+import com.fiona.mall.common.utils.Query;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.fiona.common.utils.PageUtils;
-import com.fiona.common.utils.Query;
+
 
 import com.fiona.mall.production.dao.AttrDao;
 import com.fiona.mall.production.entity.AttrEntity;
