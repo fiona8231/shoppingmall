a7f6dcef938ef1594525e0a058799c5f51882343
fiona8231 <9246068@qq.com>
2020-12-10 00:15:22 +0100

    Gateway Tree Finished

--- mall-production/src/main/java/com/fiona/mall/production/MallProductionApplication.java
+++ mall-production/src/main/java/com/fiona/mall/production/MallProductionApplication.java
@@ -3,7 +3,10 @@ package com.fiona.mall.production;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @MapperScan("com.fiona.mall.production.dao")
 @SpringBootApplication
 public class MallProductionApplication {
