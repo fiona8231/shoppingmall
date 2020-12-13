f7b080bdb2c056d50a58b10a4a3092417facee23
fiona8231 <9246068@qq.com>
2020-12-13 01:31:07 +0100

    Cloud OSS finished

--- mall-production/src/test/java/com/fiona/mall/production/MallProductionApplicationTests.java
+++ mall-production/src/test/java/com/fiona/mall/production/MallProductionApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fiona.mall.production;
 
+import com.aliyun.oss.OSSClient;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fiona.mall.production.entity.BrandEntity;
 import com.fiona.mall.production.service.BrandService;
@@ -7,20 +8,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+
 class MallProductionApplicationTests {
 
 
    @Autowired
 	BrandService brandService;
 
-	@Test
-	void contextLoads() {
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+	void contextLoads() throws FileNotFoundException {
 //		BrandEntity brandEntity = new BrandEntity();
 //		brandEntity.setName("黑莓");
 //		brandService.save(brandEntity);
@@ -31,11 +40,16 @@ class MallProductionApplicationTests {
 //
 //		brandService.updateById(brandEntity);
 
+		InputStream inputStream = new FileInputStream("/Users/xuyue/Desktop/1.jpg");
+		ossClient.putObject("dongsenshopingmall", "dongsen.jpg", inputStream);
+		ossClient.shutdown();
+		System.out.println("上传成功");
 
-		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 12l));
-        list.forEach((item)->{
-			System.out.println(item);
-		} );
+
+//		List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 12l));
+//        list.forEach((item)->{
+//			System.out.println(item);
+//		} );
 	}
 
 }
